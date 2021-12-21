79f45bb732f9f66ca9078cb540e08572642c2b68
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2021-12-21 19:59:05 +0100

    Removed password and ssid

--- WeMos_Files/boot.py
+++ WeMos_Files/boot.py
@@ -1,13 +1,17 @@
 # boot
 import network
 import gc
+
+ssid = ""
+passw = ""
+
 gc.collect()
 def do_connect():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('SL', 'Corderius2020!')
+        sta_if.connect(ssid, passw)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
